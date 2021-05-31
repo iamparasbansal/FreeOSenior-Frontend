b2442d6e56ba48bd5a4e0a2b9a8b07dffb0644f7
Chetan <61468014+Unknown39825@users.noreply.github.com>
2021-05-31 15:26:42 +0530

    Update Forget.js

--- src/components/auth/Forget.js
+++ src/components/auth/Forget.js
@@ -12,6 +12,7 @@ import Axios from "axios"
 import EmailIcon from "@material-ui/icons/EmailOutlined"
 import InputAdornment from "@material-ui/core/InputAdornment"
 import Slide from "@material-ui/core/Slide"
+import PasswordIcon from "@material-ui/icons/LockOpen"
 
 const useStyles = makeStyles(theme => ({
   fields: {
@@ -45,11 +46,17 @@ export default ({
   setTransition,
 }) => {
   const classes = useStyles()
-  const BASE_URL = "https://api.freeosenior.in"
+  const BASE_URL = "https://free-o-senior.herokuapp.com"
 
   const [email, setEmail] = useState("")
   const [emailError, setEmailError] = useState(false)
   const [emailHelperText, setEmailHelperText] = useState("")
+  const [send, setSend] = useState(false);
+  const [otp, setOtp] = useState("");
+  const [password, setPassword] = useState("")
+  const [passwordError, setPasswordError] = useState(false)
+  const [passwordHelperText, setPasswordHelperText] = useState("")
+   const [submitting, setSubmitting] = useState(!true)
 
   function TransitionDown(props) {
     return <Slide {...props} direction="down" />
@@ -67,7 +74,7 @@ export default ({
         email: email,
       }
       try {
-        await Axios.post(`${BASE_URL}/api/v1/user/forgot-password`, user, {
+        await Axios.put(`${BASE_URL}/user/forgot`, user, {
           origin: true,
           mode: "cors",
         }).then(res => {
@@ -75,19 +82,55 @@ export default ({
           setSnackColor("#4CAF50")
           setTransition(() => TransitionDown)
           setSnackbarmsg("Reset Link has been sent to your email.")
+          setSend(true);
         })
       } catch (err) {
         if (!(err.response === undefined)) {
           setSnackbar(true)
           setSnackColor("#F44336")
           setTransition(() => TransitionDown)
-          setSnackbarmsg(err.response.data.message)
+          setSnackbarmsg(err.response.data.error)
+        }
+      }
+    }
+  }
+  const onSubmit2 = async event => {
+    if (emailError === true) {
+      console.log("Unauthorized Access")
+    } else {
+      setSnackbar(true)
+      setSnackColor("#4CAF50")
+      setTransition(() => TransitionDown)
+      setSnackbarmsg("Verifying Email Address. Please Wait...")
+      const user = {
+        email: email,
+        password:password,
+        otp:otp
+      }
+      try {
+        await Axios.put(`${BASE_URL}/user/otp/verify`, user, {
+          origin: true,
+          mode: "cors",
+        }).then(res => {
+          setSnackbar(true)
+          setSnackColor("#4CAF50")
+          setTransition(() => TransitionDown)
+          setSnackbarmsg(res.data?.msg)
+          setSend(true);
+        })
+      } catch (err) {
+        if (!(err.response === undefined)) {
+          setSnackbar(true)
+          setSnackColor("#F44336")
+          setTransition(() => TransitionDown)
+          setSnackbarmsg(err.response.data.error)
         }
       }
     }
   }
 
-  const validator = () => {
+  const validator1 = () => {
+    
     if (email === "") {
       setEmailError(true)
       setEmailHelperText("This field is required")
@@ -96,6 +139,39 @@ export default ({
       setEmailHelperText("")
       onSubmit()
     }
+
+  }
+  const validator2 = () => {
+    let valid=true;
+    if (email === "") {
+      setEmailError(true)
+      setEmailHelperText("This field is required")
+    } else {
+      setEmailError(false)
+      setEmailHelperText("")
+      
+    }
+
+    if (password === "") {
+       valid = false
+       setPasswordError(true)
+       setPasswordHelperText("This field is required")
+     } else if ((password.length < 8) | (password.length > 15)) {
+       valid = false
+       setPasswordError(true)
+       setPasswordHelperText(
+         "Password length should be between 8 and 15 characters "
+       )
+     } else {
+       setPasswordError(false)
+       setPasswordHelperText("")
+     }
+
+     if (!valid) {
+       return
+     }
+     setSubmitting(true)
+     onSubmit2()
   }
   return (
     <>
@@ -114,16 +190,30 @@ export default ({
             FORGOT YOUR PASSWORD?
           </Typography>
         </Grid>
-        <Grid item>
-          <Typography
-            variant="subtitle2"
-            style={{ fontWeight: 500, color: "#1C3B6B" }}
-          >
-            Don't worry! Just fill in your Email and we'll send you a link to
-            reset your password.
-          </Typography>
-        </Grid>
+        {!send && (
+          <Grid item>
+            <Typography
+              variant="subtitle2"
+              style={{ fontWeight: 500, color: "#1C3B6B" }}
+            >
+              Don't worry! Just fill in your Email and we'll send you a link to
+              reset your password.
+            </Typography>
+          </Grid>
+        )}
+
+        {send && (
+          <Grid item>
+            <Typography
+              variant="subtitle2"
+              style={{ fontWeight: 500, color: "#1C3B6B" }}
+            >
+              Enter the OTP and verify
+            </Typography>
+          </Grid>
+        )}
       </Grid>
+
       <FormControl fullWidth error={emailError}>
         <TextField
           variant="outlined"
@@ -150,18 +240,96 @@ export default ({
         />
         {emailError && <FormHelperText>{emailHelperText}</FormHelperText>}
       </FormControl>
-      <Button
-        fullWidth
-        variant="contained"
-        style={{
-          margin: "25px 0px",
-          backgroundColor: "#1C3B6B",
-          color: "#fff",
-        }}
-        onClick={validator}
-      >
-        Send Reset Link
-      </Button>
+
+      {send && (
+        <FormControl fullWidth error={passwordError}>
+          <TextField
+            variant="outlined"
+            margin="dense"
+            className={classes.fields}
+            required
+            fullWidth
+            name="password"
+            type="password"
+            error={passwordError}
+            onChange={e => setPassword(e.target.value)}
+            placeholder="Password"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <PasswordIcon
+                    fontSize="small"
+                    color={passwordError ? "error" : "action"}
+                  />
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          {passwordError && (
+            <FormHelperText>{passwordHelperText}</FormHelperText>
+          )}
+        </FormControl>
+      )}
+      {send && (
+        <FormControl fullWidth e>
+        <TextField
+          variant="outlined"
+          margin="dense"
+          required
+          fullWidth
+          id="otp"
+         
+          onChange={e => setOtp(e.target.value)}
+          placeholder="OTP"
+          className={classes.fields}
+          name="otp"
+          autoComplete="otp"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <EmailIcon
+                  fontSize="small"
+                  color={emailError ? "error" : "action"}
+                />
+              </InputAdornment>
+            ),
+          }}
+        />
+        
+      </FormControl>
+        
+      )}
+
+      {!send && (
+        <Button
+          fullWidth
+          variant="contained"
+          style={{
+            margin: "25px 0px",
+            backgroundColor: "#1C3B6B",
+            color: "#fff",
+          }}
+          onClick={validator1}
+        >
+          Send Reset Link
+        </Button>
+      )}
+
+      {send && (
+        <Button
+          fullWidth
+          variant="contained"
+          style={{
+            margin: "25px 0px",
+            backgroundColor: "#1C3B6B",
+            color: "#fff",
+          }}
+          onClick={validator2}
+        >
+          Verify
+        </Button>
+      )}
     </>
   )
 }

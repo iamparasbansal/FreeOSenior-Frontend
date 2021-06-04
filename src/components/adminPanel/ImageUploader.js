7f133d52189f8bc79f78e564824397a58098e658
Nayan Gupta <nayanmanojgupta@gmail.com>
2021-06-04 13:28:10 +0530

    upd++

--- src/components/adminPanel/ImageUploader.js
+++ src/components/adminPanel/ImageUploader.js
@@ -1,63 +1,111 @@
-import React, { useState } from 'react';
+import React, { useState } from "react"
 
 import axiosFetch from "../../utils/axiosFetch"
-import { Button, Input, makeStyles, TextField } from "@material-ui/core"
+import {
+  Container,
+  Paper,
+  Button,
+  Input,
+  makeStyles,
+  TextField,
+  Grid,
+} from "@material-ui/core"
 import { useSelector } from "react-redux"
-import CreateIcon from "@material-ui/icons/Create"
+import PublishIcon from "@material-ui/icons/Publish"
+import Typography from "typography"
+import { DropzoneArea } from "material-ui-dropzone"
 
 const useStyles = makeStyles(theme => ({
-  
   button: {
     margin: theme.spacing(1),
-  }
+  },
+  paper: {
+    padding: "40px 20px",
+    margin: "20px auto",
+  },
+  uploadIn: {
+    padding: "40px 20px",
+  },
 }))
 
 const ImageUploader = () => {
+  const classes = useStyles()
+  const [Data, setData] = useState(undefined)
+  const [imgUrl, setimgUrl] = useState("")
 
-    const [Data, setData] = useState(undefined);
-    const [imgUrl, setimgUrl] = useState("");
+  const uploadImage = async event => {
+    const files = event.target.files
+    await setData(files[0])
+  }
 
-     const uploadImage = async event => {
-       const files = event.target.files
-       await setData(files[0])
-     }
-
-    const CloudnarySubmit = async (event) => {
-      event.preventDefault()
-      try {
-        const data = new FormData()
-        await data.append("file", Data)
-        data.append("upload_preset", "unknown39825")
-        const res = await fetch(
-          "https://api.cloudinary.com/v1_1/dvhrzmkwd/image/upload",
-          {
-            method: "POST",
-            body: data,
-          }
-        )
-        const file = await res.json()
-        setimgUrl(file.secure_url);
-        
-      } catch (error) {
-        console.log(error)
-      }
+  const CloudnarySubmit = async event => {
+    event.preventDefault()
+    try {
+      const data = new FormData()
+      await data.append("file", Data)
+      data.append("upload_preset", "unknown39825")
+      const res = await fetch(
+        "https://api.cloudinary.com/v1_1/dvhrzmkwd/image/upload",
+        {
+          method: "POST",
+          body: data,
+        }
+      )
+      const file = await res.json()
+      setimgUrl(file.secure_url)
+    } catch (error) {
+      console.log(error)
     }
+  }
 
-    return (
-      <>
-        <TextField
-          id="outlined-basic"
-          value={imgUrl}
-          name=""
-          disabled={true}
-          variant="outlined"
-          fullWidth
-        />
-
-        <Input type="file" name="file" placeholder="upload image" onChange={uploadImage} />
-        <Button onClick={CloudnarySubmit}>Upload</Button>
-      </>
-    )
+  return (
+    <>
+      <Container maxWidth="lg">
+        <Paper elevation={4} className={classes.paper}>
+          <Grid container alignItems="center" justify="center">
+            <h1 style={{ fontFamily: "arial", fontSize: 40 }}>
+              Image Uploader
+            </h1>
+          </Grid>
+          <TextField
+            id="outlined-basic"
+            value={imgUrl}
+            name=""
+            disabled={true}
+            variant="outlined"
+            fullWidth
+          />
+          <br />
+          <br />
+          <Input
+            type="file"
+            name="file"
+            placeholder="upload image"
+            onChange={uploadImage}
+            className={classes.uploadIn}
+          />
+          <DropzoneArea
+            acceptedFiles={["image/*"]}
+            dropzoneText={"Drag and drop an image here or click"}
+            onChange={files => console.log("Files:", files)}
+            onChange={files => uploadImage(files)}
+            onAlert={(message, variant) =>
+              console.log(`${variant}: ${message}`)
+            }
+          />
+          <br />
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<PublishIcon />}
+            onClick={CloudnarySubmit}
+          >
+            Upload
+          </Button>
+        </Paper>
+      </Container>
+    </>
+  )
 }
 
-export default ImageUploader;
+export default ImageUploader

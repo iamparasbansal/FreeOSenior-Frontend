ae68c51f36304eced0fd351841de855c2bd84c4e
Chetan <61468014+Unknown39825@users.noreply.github.com>
2021-06-05 18:35:54 +0530

    Create saveToken.js

---
+++ src/pages/saveToken.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import { UpdateAuthAction } from "../store/actions/Auth"
+
+export default function Savetoken(props) {
+    const [created, setCreated] = useState(false)
+    const dispatch = useDispatch();
+    useEffect( () => {
+        
+        const query = new URLSearchParams(props.location.search)
+        const token = query.get("JWT")
+        const admin = query.get("admin")
+        const userId = query.get("userId")
+        console.log(token)
+        console.log(admin)
+        const jwt = { token: token, admin: admin, userId: userId }
+        console.log(jwt)
+        localStorage.setItem("Authorization", JSON.stringify(jwt))
+        dispatch(UpdateAuthAction(jwt, true))
+        setCreated(true)
+    }, []);
+
+  if (created) {
+    return (window.location = "/")
+  }
+
+  return <div></div>
+}
